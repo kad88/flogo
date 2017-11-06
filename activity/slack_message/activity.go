8d56fa749128d7eb7805e76b8584a8d91bfb7b6f
kad88 <kaddour_dima@hotmail.com>
2017-11-06 17:40:21 +0000

    fix

--- activity/slack_message/activity.go
+++ activity/slack_message/activity.go
@@ -7,15 +7,15 @@ package slack_message
 
 import (
 
-/* "github.com/TIBCOSoftware/flogo-lib/logger"
+"github.com/TIBCOSoftware/flogo-lib/logger"
  "github.com/TIBCOSoftware/flogo-lib/core/activity"
- "github.com/nlopes/slack" */
+ "github.com/nlopes/slack"
 
 	
 )
 
 //log is the default package logger
-//var log = logger.GetLogger("KD_logger")
+var log = logger.GetLogger("KD_logger")
 
 // MyActivity is a stub for your Activity implementation
 type MyActivity struct {
@@ -35,7 +35,7 @@ func (a *MyActivity) Metadata() *activity.Metadata {
 // Eval implements activity.Activity.Eval
 func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
-	/*token := context.GetInput("token").(string)
+	token := context.GetInput("token").(string)
 	channel := context.GetInput("channelID").(string)
 	message := context.GetInput("message").(string)
 
@@ -51,12 +51,12 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	//api := slack.New("xoxp-2227445904-4843514457-260501703687-6de177cb22ae24b837b9357f5c96822b")
 	api:= slack.New(token)
-	//Gets user details
+	/* Gets user details
 	/*user, err := api.GetUserInfo("U04QTF4DF")  //user: kaddour
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
-	} ///
+	} */
 	log.Debugf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 	//fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 
@@ -67,14 +67,14 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		Pretext: "TestP",
 		Text:    "testT",
 		// Uncomment the following part to send a field too
-		///
+		/*
 			Fields: []slack.AttachmentField{
 				slack.AttachmentField{
 					Title: "a",
 					Value: "no",
 				},
 			},
-		///
+		*/
 	}
 	params.Attachments = []slack.Attachment{attachment}
 	//channelID, timestamp, err := api.PostMessage("U04QTF4DF", "Some text", params)
@@ -84,7 +84,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		return
 	}
 	//fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
-	log.Debugf("Message successfully sent to channel %s at %s", channelID, timestamp)  */
+	log.Debugf("Message successfully sent to channel %s at %s", channelID, timestamp)  
 	return true, nil
 }
 
